c68797a9ecd9f013a71f507af490a03ad2af7c20
Zach Waite <zach@waiteperspectives.com>
2023-11-03 22:28:52 -0400

    [IMP]: Add python version

---
+++ python/kata.py
@@ -0,0 +1,96 @@
+import json
+import os
+import random
+import urllib.request
+
+
+def load_config():
+    return {
+        "HOST": os.environ.get("ODOO_HOST"),
+        "PORT": os.environ.get("ODOO_PORT"),
+        "PROTOCOL": os.environ.get("ODOO_PROTOCOL"),
+        "DB": os.environ.get("ODOO_DB"),
+        "LOGIN": os.environ.get("ODOO_LOGIN"),
+        "PASSWORD": os.environ.get("ODOO_PASSWORD"),
+    }
+
+
+def json_rpc(url, method, params):
+    data = {
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+        "id": random.randint(0, 1000000000),
+    }
+    req = urllib.request.Request(
+        url=url,
+        data=json.dumps(data).encode(),
+        headers={
+            "Content-Type": "application/json",
+        },
+    )
+    reply = json.loads(urllib.request.urlopen(req).read().decode("UTF-8"))
+    if reply.get("error"):
+        raise Exception(reply["error"])
+    return reply["result"]
+
+
+def call(url, service, method, *args):
+    return json_rpc(url, "call", {"service": service, "method": method, "args": args})
+
+
+class OdooSession:
+    def __init__(self, host, port, protocol, db, user, password):
+        self.URL = f"{protocol}://{host}:{port}/jsonrpc"
+        self.DB = db
+        self.LOGIN = user
+        self.PASSWORD = password
+        self.UID = None
+
+    def login(self):
+        rs = call(self.URL, "common", "login", self.DB, self.LOGIN, self.PASSWORD)
+        self.UID = rs
+
+    def search_read(self, model, domain, fields, offset=0, limit=None, order=None):
+        return call(
+            self.URL,
+            "object",
+            "execute",
+            self.DB,
+            self.UID,
+            self.PASSWORD,
+            model,
+            "search_read",
+            domain,
+            fields,
+            offset,
+            limit,
+            order,
+        )
+
+    def get_all_employees(self):
+        return self.search_read("hr.employee", [], ["name", "job_title"])
+
+
+if __name__ == "__main__":
+    # Example: Connect to the remote server, query all the employees and print 2 records
+    config = load_config()
+    odoo = OdooSession(
+        config["HOST"],
+        config["PORT"],
+        config["PROTOCOL"],
+        config["DB"],
+        config["LOGIN"],
+        config["PASSWORD"],
+    )
+    odoo.login()
+    all_employees = odoo.get_all_employees()
+    print(all_employees[:2])
+
+# TODO: Connect to the remote Odoo server and query the number of unique job titles,
+# then print them out in order of most common to least common.
+#
+# You may use the boilerplate above or roll your own. If you feel like some light
+# reading here are some links, but don't worry, there isn't a quiz :)
+#  - https://www.odoo.com/documentation/16.0/developer/howtos/web_services.html
+#  - https://www.odoo.com/documentation/16.0/developer/reference/external_api.html
